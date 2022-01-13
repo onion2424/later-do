9859930e3c23c226a4fc4f2bf3104aa48c053dc1
らんるん <ss0302winning@gmail.com>
2022-01-13 22:34:11 +0900

    composer require linecorp/line-bot-sdk

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7a883e20b615a43bbbe2230408c9de03b6c35c44',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7a883e20b615a43bbbe2230408c9de03b6c35c44',
+            'dev_requirement' => false,
+        ),
+        'linecorp/line-bot-sdk' => array(
+            'pretty_version' => '3.6.0',
+            'version' => '3.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../linecorp/line-bot-sdk',
+            'aliases' => array(),
+            'reference' => 'e1515090eaf2106efdbdf51b1a6adf812844f54f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
