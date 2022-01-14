d8a7ca86a11142214a248e37c0c845a462820e65
らんるん <ss0302winning@gmail.com>
2022-01-15 04:30:20 +0900

    ためし

--- src/App.svelte
+++ src/App.svelte
@@ -36,7 +36,7 @@
 				url: "/id_api.php",
 				headers: { "Content-Type": 'application/x-www-form-urlencoded' },
 				responseType: "json",
-				data: {body: JSON.stringify(idToken)}
+				data: JSON.stringify({id_token: idToken})
 			}).then((response) => console.log("response body:", response.data));
 		});
 		//} else {
