c3c0e98896209ad7ac5b8e080f3d32068eed7845
らんるん <ss0302winning@gmail.com>
2022-01-15 01:22:06 +0900

    svelte対応

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php include_once("index.html"); ?>
+<?php 
+include_once("index.html"); 
+//http通信の準備
+$ch = curl_init();
+
+$url = 'https://api.line.me/oauth2/v2.1/verify';
+
+$date = [
+    'id_token' => $idToken,
+]
+
+?>
