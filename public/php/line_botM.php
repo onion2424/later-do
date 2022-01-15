d77e7d5a3afd4f04d08fd598f198241e54a9af4d
らんるん <ss0302winning@gmail.com>
2022-01-15 19:24:03 +0900

    vendor パスミス

--- public/php/line_botM.php
+++ public/php/line_botM.php
@@ -1,6 +1,6 @@
 <?php
 // Composerでインストールしたライブラリを一括読み込み
-require_once __DIR__ . '../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use LINE\LINEBot\Constant\HTTPHeader;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
