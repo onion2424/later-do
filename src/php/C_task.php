00b3868468f2da90ecf5f27a5733c6dfac2a8bd5
らんるん <ss0302winning@gmail.com>
2022-01-17 23:08:54 +0900

    クラス名変更

--- src/php/C_task.php
+++ src/php/C_task.php
@@ -4,7 +4,7 @@ namespace app\src\php;
 require_once __DIR__ . '/common_class.php';
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-class task
+class C_task
 {
 
     public function getTasks()
