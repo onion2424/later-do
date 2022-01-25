12e2334b13cbbceaf5303ce6a959b3516f4f1a06
らんるん <ss0302winning@gmail.com>
2022-01-26 05:25:56 +0900

    builderテスト

--- public/php/line_bot.php
+++ public/php/line_bot.php
@@ -96,11 +96,11 @@ foreach ($events as $event) {
                 $builder = new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
                 // ビルダーにメッセージをすべて追加
                 $message = [
-                    ['type' => 'text', 'text' => 'あとでやろうと思ったことをトークで送ってね！約「10分後」,「30分後」,「1時間後」,「3時間後」,「6時間後」,「次の日の朝の6時」にお知らせするよ！'],
-                    ['type' => 'text', 'text' => '終了したタスクはメニューの一覧から削除できるよ！'],
+                    ['あとでやろうと思ったことをトークで送ってね！約「10分後」,「30分後」,「1時間後」,「3時間後」,「6時間後」,「次の日の朝の6時」にお知らせするよ！'],
+                    ['終了したタスクはメニューの一覧から削除できるよ！']
                 ];
                 foreach($message as $msg){
-                    $builder->add($msg);
+                    $builder->add(new \LINE\LineBot\MessageBuilder\TextMessageBuilder($msg));
                 }
                 $bot->replyMessage($reply_token, $builder);
             }else{
