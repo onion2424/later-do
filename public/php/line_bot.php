76650f3e27f58d71bf04d4e1c4386a209134eb85
らんるん <ss0302winning@gmail.com>
2022-01-26 06:21:34 +0900

    1つしか送れない

--- public/php/line_bot.php
+++ public/php/line_bot.php
@@ -104,8 +104,8 @@ foreach ($events as $event) {
                  foreach($message as $msg){
                     $builder->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message));
                  }
-                //$bot->replyText($reply_token, 'かえる？');
-                $bot->replyMessage($reply_token, $builder);
+                $bot->replyText($reply_token, 'かえる？');
+                //$bot->replyMessage($reply_token, $builder);
             }else{
               $message = "タスク登録に失敗しました。もう一度送信してください。";
               try {
