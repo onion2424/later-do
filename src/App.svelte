d9a27223b91dbd7efa70908ebcef2c6c850cd308
らんるん <ss0302winning@gmail.com>
2022-01-27 19:09:20 +0900

    チェック画像取得テスト

--- src/App.svelte
+++ src/App.svelte
@@ -135,8 +135,8 @@
 				<div class="task_top">
 					<Swiper
 						on:progress={(e) => {
-							let elm = e[0].el.closest('task_wrapper').firstChild;
-							console.log(elm.firstChild.innerHTML);
+							let elm = e[0]?.el.closest('task_wrapper').firstChild;
+							console.log(elm?.firstChild.innerHTML);
 							}}
 						allowSlidePrev={false}
 						longSwipesRatio={0.2}
