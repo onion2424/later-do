faa9aedbe0bd8c0f4d358fa7bb19dbb6730dedaf
らんるん <ss0302winning@gmail.com>
2022-01-28 04:29:58 +0900

    aspect-ration試し

--- src/App.svelte
+++ src/App.svelte
@@ -133,8 +133,13 @@
 			background:url('../img/back_img.png');
    			background-size:contain;
    			height:calc(100% / 2);
-			position:absolute;;
-			width: 100%;
+			//width: 100%;
+			aspect-ratio: 1 / 1;
+			position:absolute;
+			top:0;
+			left:0;
+			right:0;
+			bottom:0;
 			margin:auto;
 			background-repeat: no-repeat;
 
