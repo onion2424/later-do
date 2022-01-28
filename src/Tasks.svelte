48e05c62efd0307d032239c8f1d9149df2be9e4b
らんるん <ss0302winning@gmail.com>
2022-01-29 05:00:27 +0900

    datetime-localがtimeを捻じ曲げるのに対応できていない

--- src/Tasks.svelte
+++ src/Tasks.svelte
@@ -216,6 +216,7 @@
         display:inline-block;
         input[type="datetime-local"]{
              width:100%;
+             height:100%;
              appearance: none;
              position: absolute;
              top: 0;
