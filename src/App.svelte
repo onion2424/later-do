0db29d0134d2b503dd39781ab56dae64ca467ff3
らんるん <ss0302winning@gmail.com>
2022-01-29 06:38:12 +0900

    画像試し

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import axios from "axios";
 	import Tasks from "./Tasks.svelte";
-    //--------------定数-------------
+	//--------------定数-------------
 	const MODE_LATER = 1;
-    const MODE_NEXT = 2;
+	const MODE_NEXT = 2;
 	//------------プロパティ-------------------
 	let todos = [];
 	let isInClient = false;
@@ -14,11 +14,11 @@
 
 	//--------------関数----------------------
 	//** 送信日付設定*/
-	function setDateTodo(e){
+	function setDateTodo(e) {
 		let taskNo = e.detail.taskNo;
 		let time = e.detail.time;
 
-		if(Number.isNaN(new Date(time).getDate())){
+		if (Number.isNaN(new Date(time).getDate())) {
 			//不正な時間なら時間をリセットしてリターン
 			let task = todos.find((val) => val.taskNo == taskNo);
 			task.time = "";
@@ -28,7 +28,11 @@
 		axios
 			.post(
 				"/setdate-task",
-				JSON.stringify({ id_token: idToken, taskNo: taskNo, time: time})
+				JSON.stringify({
+					id_token: idToken,
+					taskNo: taskNo,
+					time: time,
+				})
 			)
 			.then((res) => {
 				//ディープコピーをする
@@ -42,19 +46,18 @@
 			})
 			.catch((e) => {
 				//閉じる
-				Promise.resolve()
-					.then(() => alert(e));
+				Promise.resolve().then(() => alert(e));
 				//	.then(() => window.open("about:blank", "_self").close());
 			});
-	} 
+	}
 
 	//** タスクトグル*/
-	function toggleTodo(e){
+	function toggleTodo(e) {
 		let taskNo = e.detail.taskNo;
 		axios
 			.post(
 				"/toggle-task",
-				JSON.stringify({ id_token: idToken, taskNo: taskNo})
+				JSON.stringify({ id_token: idToken, taskNo: taskNo })
 			)
 			.then((res) => {
 				//ディープコピーをする
@@ -65,13 +68,22 @@
 					let task = temp.find((val) => val.taskno == taskNo);
 					task.isnexttime = !task.isnexttime; //フラグを反転
 					//時間をセット
-					if(mode === MODE_LATER){
+					if (mode === MODE_LATER) {
 						task.time = "";
-					}else{
+					} else {
 						// 2022-01-01T12:00 のような形(16文字)に整形する
 						let setTime = new Date();
-						task.time = setTime.getFullYear() + "-" + ( '0' + Number(setTime.getMonth() + 1)).slice(-2) + "-" + ('0' + setTime.getDate()).slice(-2) 
-						+ "T" + ('0' + setTime.getHours()).slice(-2) + ":" + ('0' + setTime.getMinutes()).slice(1, 2) + '0';
+						task.time =
+							setTime.getFullYear() +
+							"-" +
+							("0" + Number(setTime.getMonth() + 1)).slice(-2) +
+							"-" +
+							("0" + setTime.getDate()).slice(-2) +
+							"T" +
+							("0" + setTime.getHours()).slice(-2) +
+							":" +
+							("0" + setTime.getMinutes()).slice(1, 2) +
+							"0";
 						console.log(task.time);
 					}
 					//todosを入れ替えて再描画させる
@@ -82,11 +94,10 @@
 			})
 			.catch((e) => {
 				//閉じる
-				Promise.resolve()
-					.then(() => alert(e));
+				Promise.resolve().then(() => alert(e));
 				//	.then(() => window.open("about:blank", "_self").close());
 			});
-	} 
+	}
 
 	//** タスク削除   */
 	function deleteTodo(e) {
@@ -111,8 +122,7 @@
 			})
 			.catch((e) => {
 				//閉じる
-				Promise.resolve()
-					.then(() => alert(e));
+				Promise.resolve().then(() => alert(e));
 				//	.then(() => window.open("about:blank", "_self").close());
 			});
 	}
@@ -151,15 +161,17 @@
 					})
 					.catch((e) => {
 						//閉じる
-						Promise.resolve()
-							.then(() => alert(e));
-							//.then(() =>
-							//	window.open("about:blank", "_self").close()
-							//);
+						Promise.resolve().then(() => alert(e));
+						//.then(() =>
+						//	window.open("about:blank", "_self").close()
+						//);
 					});
 			});
-		}else{
-			todos = [{time: "今日", task: 'あいうえお', isnexttime: false}, {time: "明日", task: 'かきくけこ', isnexttime: true}];
+		} else {
+			todos = [
+				{ time: "今日", task: "あいうえお", isnexttime: false },
+				{ time: "明日", task: "かきくけこ", isnexttime: true },
+			];
 		}
 	});
 </script>
@@ -170,15 +182,24 @@
 			<p>...データ取得中</p>
 		{:then}
 			{#if todos.length > 0}
-			  <Tasks todos={todos} bind:mode={mode} on:delete={deleteTodo} on:toggle={toggleTodo} on:setdate={setDateTodo}/>
+				<Tasks
+					{todos}
+					bind:mode
+					on:delete={deleteTodo}
+					on:toggle={toggleTodo}
+					on:setdate={setDateTodo}
+				/>
 			{:else if isLoadEnd}
-			<div class="nothing_task">
-				<div class="background">
-					<div>
-						<p>あとでやることはありません。<br/>LINEではなしかけてとうろくしてね！</p>
+				<div class="nothing_task">
+					<div class="background">
+						<div>
+							<p>
+								あとでやることはありません。<br
+								/>LINEではなしかけてとうろくしてね！
+							</p>
+						</div>
 					</div>
 				</div>
-			</div>	
 			{/if}
 		{/await}
 	{:else}
@@ -188,69 +209,96 @@
 			/>LINEアプリ内でこのメッセージが表示されているのなら、リロードによってなおる場合があります。
 		</p>
 		{#if todos.length > 0}
-		  <Tasks todos={todos} bind:mode={mode}/>
-	    {/if}
+			<Tasks {todos} bind:mode />
+		{/if}
 	{/if}
 </main>
 <menu>
 	<ul>
-		<li><button on:click={()=>{mode = MODE_LATER}}><div class="later"><span>あとで</span></div></button>
-		<li><button on:click={() => {mode=MODE_NEXT}}><div class="nexttime"><span>こんど</span></div></button>
+		<li>
+			<button
+				on:click={() => {
+					mode = MODE_LATER;
+				}}><div class="later"><span>あとで</span></div></button
+			>
+		</li>
+		<li>
+			<button
+				on:click={() => {
+					mode = MODE_NEXT;
+				}}><div class="nexttime"><span>こんど</span></div></button
+			>
+		</li>
 	</ul>
 </menu>
+
 <style lang="scss">
 	$line_color: #06c755;
 	main {
 		margin: 0 auto;
 		height: calc(100% - 50px);
 		//marginをはみださせない
-		display:flow-root;
-		p{
+		display: flow-root;
+		p {
 			color: $line_color;
-			text-align:center;
+			text-align: center;
 		}
 	}
-	menu{
+	menu {
 		height: 50px;
 		background-color: white;
-		margin:0;
+		margin: 0;
 		padding: 0;
-		position:absolute;
+		position: absolute;
 		bottom: 0;
 		width: 100%;
 		box-sizing: border-box;
 		border-top: 1px solid #dbdbdb6e;
-		ul{
+		ul {
 			display: flex;
 			margin: 0;
 			padding: 0;
 			height: 100%;
-			li{
+			li {
 				list-style: none;
-				width:calc(100% / 2);
-				button{
-					height:100%;
-					width:100%;
+				width: calc(100% / 2);
+				button {
+					height: 100%;
+					width: 100%;
 					display: inline-block;
-					border:none;
+					border: none;
 					cursor: pointer;
 					background-color: rgba(255, 255, 255, 0);
 					margin: 0;
 					padding: 0;
-					div.later{
-						height:100%;
-						width:100%;
-						background:url('../img/translate_later.png');
-   						background-size:contain;
-			  			background-repeat: no-repeat;
+					div.later {
+						height: 100%;
+						aspect-ratio: 1 / 1;
+						background: url("../img/translate_later.png");
+						background-size: contain;
+						//中央に配置
+						position: absolute;
+						top: 0;
+						left: 0;
+						right: 0;
+						bottom: 0;
+						background-repeat: no-repeat;
+						margin: auto;
 						background-color: red;
 					}
-					div.nexttime{
-						height:100%;
-						width:100%;
-						background:url('../img/translate_nexttime.png');
-   						background-size:contain;
-			 		    background-repeat: no-repeat;
+					div.nexttime {
+						height: 100%;
+						aspect-ratio: 1 / 1;
+						background: url("../img/translate_nexttime.png");
+						background-size: contain;
+						//中央に配置
+						position: absolute;
+						top: 0;
+						left: 0;
+						right: 0;
+						bottom: 0;
+						background-repeat: no-repeat;
+						margin: auto;
 						background-color: blue;
 					}
 				}
@@ -258,35 +306,34 @@
 		}
 	}
 
-	div.nothing_task{
-		height:100%;
-		width:100%;
-	    div.background{
-			background:url('../img/back_img.png');
-   			background-size:contain;
-   			height:calc(100% / 2);
+	div.nothing_task {
+		height: 100%;
+		width: 100%;
+		div.background {
+			background: url("../img/back_img.png");
+			background-size: contain;
+			height: calc(100% / 2);
 			aspect-ratio: 1 / 1;
 			//中央に配置
-			position:absolute;
-			top:0;
-			left:0;
+			position: absolute;
+			top: 0;
+			left: 0;
 			right: 0;
-			bottom:0;
+			bottom: 0;
 			background-repeat: no-repeat;
 			margin: auto;
-			div{
-				position:relative;
+			div {
+				position: relative;
 				height: 100%;
 				width: 100%;
 			}
-			p{
-				position :absolute;
-				left:50%;
-				bottom:0;
-				width:100%;
+			p {
+				position: absolute;
+				left: 50%;
+				bottom: 0;
+				width: 100%;
 				transform: translate(-50%, 0);
 			}
 		}
 	}
-
 </style>
